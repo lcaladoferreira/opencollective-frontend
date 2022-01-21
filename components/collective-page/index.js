77ebc718e38fb982deea40efef5551d8fccea2e5
Sudharaka <sudharakap@mun.ca>
2022-01-21 06:39:21 +0530

    feat: hide sections on navbar expand on mobile

--- components/collective-page/index.js
+++ components/collective-page/index.js
@@ -273,7 +273,7 @@ class CollectivePage extends Component {
           showSelectedCategoryOnMobile
         />
 
-        <div ref={this.sectionsContainerRef}>
+        <div ref={this.sectionsContainerRef} className="sectionsContainer">
           {isEmpty(sections) ? (
             <SectionEmpty collective={this.props.collective} />
           ) : (
